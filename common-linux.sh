4ec4ea30a9d4b354f932b9eb1d51c34c6d479859
Nguyen Van Khai <43227792+nvkhai603@users.noreply.github.com>
2025-04-15 22:48:53 +0700

    Create common-linux.sh

---
+++ common-linux.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Đây là file chứa các script chung cho tất cả các máy chủ
+
+# Cài đặt các gói cần thiết
+# htop
+sudo apt-get install htop
+# xclip
+sudo apt-get update && sudo apt-get install -y xclip
+
+# Cài đặt clouldflare
+sudo mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
+echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+sudo apt-get update && sudo apt-get install cloudflared
