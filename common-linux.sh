8035b9132d4316417f9302688d2919e82c13030b
Nguyen Van Khai <43227792+nvkhai603@users.noreply.github.com>
2025-04-16 17:08:37 +0700

    Update common-linux.sh

--- common-linux.sh
+++ common-linux.sh
@@ -5,8 +5,12 @@
 sudo apt-get install -y htop
 sudo apt-get install -y xclip
 
-# Cài đặt clouldflare
+# Add cloudflare gpg key
 sudo mkdir -p --mode=0755 /usr/share/keyrings
-sudo curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
-sudo echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
-sudo apt-get install -y cloudflared
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
+
+# Add this repo to your apt repositories
+echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+
+# install cloudflared
+sudo apt-get update && sudo apt-get install cloudflared
