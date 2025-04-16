256cdd124dd86d0edfaaa3e02937ba4dfe934195
Nguyen Van Khai <43227792+nvkhai603@users.noreply.github.com>
2025-04-16 17:02:34 +0700

    Update common-linux.sh

--- common-linux.sh
+++ common-linux.sh
@@ -7,6 +7,6 @@ sudo apt-get install -y xclip
 
 # Cài đặt clouldflare
 sudo mkdir -p --mode=0755 /usr/share/keyrings
-sydo curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
+sudo curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
 sudo echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
 sudo apt-get install -y cloudflared
